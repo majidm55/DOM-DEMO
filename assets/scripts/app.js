0dfdbe17f3f7bb5fc70173f8d8a2365446c4c3d6
Majid <mohammadmajid55@gmail.com>
2020-07-09 15:41:56 -0400

    rendering movies to the UI

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,10 +3,36 @@ const modal = document.getElementById('add-modal');
 const backdrop = document.getElementById('backdrop');
 const cancelAddMovieBtn = modal.querySelector('.btn--passive');
 const addMovieBtn = cancelAddMovieBtn.nextElementSibling;
-
 const userInputs = modal.querySelectorAll('input');
+const entryText = document.getElementById('entry-text');
 
 const movies = [];
+
+const updateUI = () => {
+  if(movies.length === 0 ) {
+    entryText.style.display = 'block';
+  } else {
+    entryText.style.display = 'none';
+  }
+}
+
+const renderNewMovie = (title, imageUrl, rating) => {
+  const newElMovie = document.createElement('li');
+  newElMovie.className = 'movie-element';
+  newElMovie.innerHTML = `
+    <div class="movie-element__image">
+    <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+      <h2>${title}</h2>
+      <p>${rating}/5 stars</p>
+    
+    </div>
+  
+  `
+  const listRoot = document.getElementById('movie-list');
+  listRoot.append(newElMovie);
+};
 
 const addMovieHandler = () => {
   const titleValue = userInputs[0].value;
@@ -32,6 +58,8 @@ const addMovieHandler = () => {
   console.log(movies);
   toggleModal();
   clearInput();
+  updateUI();
+  renderNewMovie(newMovie.title, newMovie.imageUrl, newMovie.rating);
 };
 
 // let addMovieInfo = {
